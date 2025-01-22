c676426537817421876288f7d96d4cb611a50676
Marcos Arias <marcos.augusto.arias@gmail.com>
2025-01-22 14:06:50 -0300

    Subiendo proyecto inicial

---
+++ js/commons.js
@@ -0,0 +1,55 @@
+﻿var nada = !function ($) {
+    $(function () {
+        window.prettyPrint && prettyPrint();
+        initZeroClipboard();
+    });
+}(window.jQuery);
+
+//#region Methods
+
+function initZeroClipboard() {
+
+    // Source: http://zeroclipboard.org/
+    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'));
+    var htmlBridge = $('#global-zeroclipboard-html-bridge');
+
+    // Handlers for ZeroClipboard.
+    zeroClipboard.on('ready', function (event) {
+        htmlBridge
+            .data('placement', 'top')
+            .attr('title', 'Copy to clipboard')
+            .tooltip();
+
+        // Event Copy.
+        zeroClipboard.on('copy', function (event) {
+            var ret = [],
+                $highlight = $(event.target).parent().nextAll('.highlight').first();
+
+            // Se concatena el contenido de cada tag <li> para poder imprimirlo linea a linea.
+            // Usando el método text de jQuery devolvía tod o en una unica línea.
+            var $li = $highlight.find('li');
+            if ($li.length) {
+                $li.each(function (idx, item) {
+                    ret.push($(item).text());
+                });
+            } else {
+                ret.push($highlight.text());
+            }
+
+            event.clipboardData.setData("text/plain", ret.join('\n'));
+        });
+
+        // Event AfteCopy.
+        zeroClipboard.on('aftercopy', function () {
+            // Notify copy success and reset tooltip title.
+            htmlBridge
+                .attr('title', 'Copied!')
+                .tooltip('fixTitle')
+                .tooltip('show')
+                .attr('title', 'Copy to clipboard')
+                .tooltip('fixTitle');
+        });
+    });
+}
+
+//#endregion
---
+++ js/prettyPrint.highlightLines.js
@@ -0,0 +1,53 @@
+﻿!(function ($) {
+    $.fn.highlightLines = function (options) {
+        var settings = $.extend({
+            className: 'highlight-line'
+        }, options);
+        return this.each(function () {
+            // Busca todos los elementos "li" contenidos en el bloque.
+            var $el = $(this),
+                $elements = $el.find('li'),
+                clsAttr = $el.attr('class') || '';
+            if (!$elements.length) {
+                $elements = $el;
+            }
+            // Busca la palabra highlight-line dentro del texto de l apropiedad "class"
+            // y las posiciones de los corchetes (dentro de los cuales se encuentran 
+            // enumeradas las líneas a resaltar.
+            var key = 'highlight-line',
+                numsConfig = clsAttr && (clsAttr.indexOf(key) > -1) ? clsAttr.substring(clsAttr.indexOf(key)) || '' : $el.data(key) || '',
+                reNums = /(\[)?(\d+-\d+|\d+)(\])?/g,
+                lines = [],
+                num;
+            while (num = reNums.exec(numsConfig)) {
+                var reNum = /\d+/g,
+                    lineAux = [],
+                    pos = (num.length == 4) ? 2 : 1,
+                    item;
+                while (item = reNum.exec(num[pos])) {
+                    var value = parseInt(item),
+                        numAnt = (lineAux.length) ? lineAux[lineAux.length - 1] : value;
+                    if (value != numAnt) {
+                        for (var i = numAnt + 1; i <= value; i++) {
+                            lineAux.push(i);
+                        }
+
+                    } else {
+                        lineAux.push(value);
+                    }
+                }
+
+                lines = lines.concat(lineAux);
+            }
+
+            for (var idx = 0, totalLines = lines.length; idx < totalLines; idx++) {
+                var idxReal = lines[idx] - 1;
+                // En cada ListItem que corresponda a una línea especificada, 
+                // se aplica el estilo .resaltarLinea a todos los spans 
+                // contenidos (excepto al primer span, que es utilizado como 
+                // espaciador).
+                $elements.eq(idxReal).find('span:not(.pln:first)').addClass(settings.className);
+            }
+        });
+    };
+}(jQuery, prettyPrint));
